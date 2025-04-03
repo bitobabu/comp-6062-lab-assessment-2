ee4322b36164efb19d44f3d3db238449fd7876a1
bitobabu <bitobabu246@gmail.com>
2025-04-03 19:51:02 -0400

    lab assessment

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+const app =Vue.createApp({
+    data(){
+        return{
+        myFullname:'Bito Babu',
+        pressureMonitorValue :65,
+        pressureMonitorLevels: [
+            { id: 'danger-low', color: 'danger', label: 'Danger - Low!', min: 0, max:10 },
+            { id: 'warn-low', color: 'warning', label: 'Warning - Low', min: 11, max:30 },
+            { id: 'safe', color: 'safe', label: 'Safe', min: 31, max: 70 },
+            { id: 'warn-high', color: 'warning', label: 'Warning - High', min: 71, max: 90 },
+            { id: 'danger-high', color: 'danger', label: 'Danger - High', min: 91, max: 100 }
+            ]
+            
+
+        };
+    },
+
+    methods:{
+        isActive(pressurelevel){
+            return this.pressureMonitorValue>=pressurelevel.min&&this.pressureMonitorValue<=pressurelevel.max
+        }
+    }
+});
+
+app.mount('#app');
